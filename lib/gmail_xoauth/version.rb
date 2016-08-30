76ae1fffa3ffe49f02a6b08d10a70735bb2e5002
Rafal Boni <rafal@yesware.com>
2016-08-30 13:42:36 -0400

    SMTP: on 3xx response to 'AUTH XOAUTH' send CR-LF to get actual error.
    See the note in https://developers.google.com/gmail/xoauth2_protocol which
    explains this bit ('note about SMTP protocol exchange').  Apparently, we'd
    all missed it for years.

--- lib/gmail_xoauth/version.rb
+++ lib/gmail_xoauth/version.rb
@@ -1,3 +1,3 @@
 module GmailXoauth
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 end
--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 require 'rubygems'
 require 'test/unit'
-require 'mocha'
+require 'mocha/test_unit'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
